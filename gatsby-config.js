eb994fa7c5088294114947a1da331fcb8f4820de
carter scadden <carter@scaddenfamily.com>
2019-07-02 11:50:27 -0600

    cms should be up, or at least ability to login into the cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,16 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        /**
+         * One convention is to place your Netlify CMS customization code in a
+         * `src/cms` directory.
+         */
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
---
+++ src/cms/cms.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const cms = (props) => {
+  console.log(props);
+  return (
+    <section>
+      <h1>Hello world</h1>
+    </section>
+  );
+};
+
+export default cms;
